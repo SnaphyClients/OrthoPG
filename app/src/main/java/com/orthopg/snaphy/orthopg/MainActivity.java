63a815e7d48be03aa11d5e497656ef6476796d3a
nikita <nikita@snaphy.com>
2017-05-05 14:49:11 +0530

    Saving changes and editing ravi's sir laptop

--- app/src/main/java/com/orthopg/snaphy/orthopg/MainActivity.java
+++ app/src/main/java/com/orthopg/snaphy/orthopg/MainActivity.java
@@ -1087,7 +1087,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChange,
     }
 
     //For getting payment status from payu money sdk
-    @Override
+    /*@Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
@@ -1126,27 +1126,9 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChange,
                 showDialogMessage("User returned without login");
             }
         }
-    }
+    }*/
 
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(Constants.TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            if(acct.getIdToken() != null) {
-                Log.v(Constants.TAG, acct.getIdToken());
-                //BackgroundService.setAccessToken(acct.getIdToken());
-                //mainActivity.replaceFragment(R.layout.fragment_mciverification, null);
-                Presenter.getInstance().addModel(Constants.GOOGLE_ACCESS_TOKEN, acct.getIdToken());
-                mainActivity.replaceFragment(R.layout.fragment_mciverification, null);
-                //sendTokenToServer(acct.getIdToken());
-            }else{
-                TastyToast.makeText(mainActivity.getApplicationContext(), Constants.ERROR_MESSAGE, TastyToast.LENGTH_SHORT, TastyToast.ERROR);
-            }
-        } else {
-            TastyToast.makeText(mainActivity.getApplicationContext(), Constants.ERROR_MESSAGE, TastyToast.LENGTH_SHORT, TastyToast.ERROR);
-        }
-    }
+
 
 
     //Verifying the payment from server side
